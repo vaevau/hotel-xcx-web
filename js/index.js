fa432dd78a11fc1d229c2b15d9a69ed649b6b1d1
ArivnC <ArivnC@qq.com>
2020-12-04 23:21:25 +0800

    黑色遮罩和部分js

---
+++ js/index.js
@@ -0,0 +1,49 @@
+// var oFilterMenu = document.getElementById('filter-menu');
+// var aLi = oFilterMenu.getElementsByTagName('div');
+// var oSortDis = document.getElementById('sort-distance');
+// var aSpan = oFilterMenu.getElementsByTagName('div');
+// var oSortPlace = document.getElementById('sort-place-detail');
+
+
+// for (var i = 0;i<aSpan.length; i++){
+//    aSpan[i].index = i;
+//     aSpan[i].onmouseover = function(){
+//         this.className = 'active';
+//         if(this.index == 0){
+//             oSortDis.style.display = 'block';
+//         }else if (this.index == 1){
+//             oSortPlace.style.display = 'block';
+//         }      
+//     }
+   
+// }
+
+
+var list = [
+    {
+        name: '距离排序',
+        active: true
+    },
+    {
+        name: '品牌价格'
+    },
+    {
+        name: '其他筛选'
+    },
+    {
+        name: '位置区域'
+    }
+]
+var oFilter = document.getElementById('filter-menu');
+console.log(oFilter.innerHTML);
+var temp = '';
+for (var index = 0; index < list.length; index++) {
+    var el = list[index];
+    // console.log(el.name);
+    var odiv = '<div>'+el.name+'</div>'
+    // console.log(odiv);
+    temp += odiv
+}
+console.log(temp);
+
+oFilter.innerHTML = temp;
