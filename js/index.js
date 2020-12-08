6f5868958d3676231ef06f732ec5a69c7f0c796b
ArivnC <ArivnC@qq.com>
2020-12-08 23:31:22 +0800

    滚动条监听

--- js/index.js
+++ js/index.js
@@ -19,31 +19,42 @@
 // }
 
 
-var list = [
-    {
-        name: '距离排序',
-        active: true
-    },
-    {
-        name: '品牌价格'
-    },
-    {
-        name: '其他筛选'
-    },
-    {
-        name: '位置区域'
-    }
-]
-var oFilter = document.getElementById('filter-menu');
-console.log(oFilter.innerHTML);
-var temp = '';
-for (var index = 0; index < list.length; index++) {
-    var el = list[index];
-    // console.log(el.name);
-    var odiv = '<div>'+el.name+'</div>'
-    // console.log(odiv);
-    temp += odiv
-}
-console.log(temp);
+// var list = [
+//     {
+//         name: '距离排序',
+//         active: true
+//     },
+//     {
+//         name: '品牌价格'
+//     },
+//     {
+//         name: '其他筛选'
+//     },
+//     {
+//         name: '位置区域'
+//     }
+// ]
+// var oFilter = document.getElementById('filter-menu');
+// console.log(oFilter.innerHTML);
+// var temp = '';
+// for (var index = 0; index < list.length; index++) {
+//     var el = list[index];
+//     // console.log(el.name);
+//     var odiv = '<div>'+el.name+'</div>'
+//     // console.log(odiv);
+//     temp += odiv
+// }
+// console.log(temp);
 
-oFilter.innerHTML = temp;
+// oFilter.innerHTML = temp;
+
+
+
+//添加酒图片评论等店数据
+
+
+// var oHotelMsg = hotelMsg.hotelData.map(function(d){
+//     var oHotelImg = d.pic-hotel, oHotelImg2 = d.pic-hotel02,
+//     oComment = d.comment, oPrice = d.price, oRcmd = d.rcmd,
+//     oDistance = d.distance;
+// })
---
+++ js/json-data.js
@@ -0,0 +1,14 @@
+var hotelMsg = 
+{
+    "hotelData":
+    [
+        {
+            "picHotel":"img/pic-hotel.jpg",
+            "picHhotel2":"img/pic-hotel02.jpg",
+            "rcmdHotel":"如家精选, 上海浦东地区",
+            "comment":"服务周到周到,一句话推荐理由荐理由理由荐理...",
+            "distance":"2km",
+            "price":"302"
+        }
+    ]
+} 
